0033fb0237bf13cbadbd0b6a6edddb1456f82b07
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-09-18 02:16:33 +0530

    update run script with stdin

--- script/run.py
+++ script/run.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+input_path = input("Enter the directory path containing profiles to be parsed: ")
+output_path = input("Enter the directory path where reports will be dumped: ")
+filters = input("Profile properties for which reports will be generated (optional): ")
+print(input_path)
+print(output_path)
+print(filters)
+
+# execute maven package command on cmd
+# redirect cmd output to python stdout
+# execute jar file from target dir with parameters using subprocess
+# show some message 
